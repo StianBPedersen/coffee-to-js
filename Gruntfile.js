f94661d9a071dded680f0917e691bdb253440af1
StianBPedersen <stianb@kompis.com>
2014-03-18 17:09:45 +0100

    coffee to js first edition

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		clean: {
+			files: ['<app/javascripts/js/script/script.js']
+		},
+		coffee: {
+			compile: {
+				options: {
+					bare: true,
+					separator: ";"
+				},
+				files: {
+					'app/javascripts/js/script.js': ['app/javascripts/coffeescript/*.coffee']
+				}
+			}
+		},
+		uglify: {
+			options: {
+				banner: '/* by <%= pkg.author %> at <%= grunt.template.today("dd-mm-yyyy") %> */',
+				mangle: true,
+				sourceMap: true,
+				compress: {
+					drop_console: true
+				}
+			},
+			target: {
+				files: {
+					'app/javascripts/js/script.min.js': ['app/javascripts/js/script.js']
+				}
+			}
+		},
+		watch: {
+			scripts: {
+				files: ['**/*.coffee'],
+				tasks: ['clean', 'coffee', 'uglify'],
+				options: {
+					livereload: true
+				},
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-coffee');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('watcher', ['watch']);
+};
---
+++ app/javascripts/coffeescript/script2.coffee
@@ -0,0 +1,6 @@
+###
+	Detta er fil nummer 2
+###
+
+square = (x) -> x * x
+cube   = (x) -> square(x) * x
